2bc8168d9d9caa4beb5d6d343a705007e23bd40f
mkhodroo <info@behin.energy>
2024-05-26 13:24:27 +0330

    Update Access.php

--- packages/behin-init/src/app/Http/Middleware/Access.php
+++ packages/behin-init/src/app/Http/Middleware/Access.php
@@ -2,8 +2,10 @@
 
 namespace BehinInit\App\Http\Middleware;
 
+use BehinInit\App\Http\Controllers\AccessController;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Access
 {
@@ -16,6 +18,19 @@ class Access
      */
     public function handle(Request $request, Closure $next)
     {
+        $guards = empty($guards) ? [null] : $guards;
+
+        if(!Auth::id()){
+            return abort(403, 'ابتدا وارد شوید');
+        }
+        $route = $request->route()->getName() ? $request->route()->getName() : $request->route()->uri();
+        $a = new AccessController($route);
+        if(!$a->check()){
+            return abort(403, "Forbidden For Route: " . $route);
+        }
+
+        
+
         return $next($request);
     }
 }
