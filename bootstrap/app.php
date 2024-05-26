77654e4a37994580f79b3b6a49bee6aa381c1d70
mkhodroo <info@behin.energy>
2024-05-26 13:42:24 +0330

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(Access::class);
+        // $middleware->append(Access::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
