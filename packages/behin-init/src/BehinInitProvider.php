25a21eaad49e1771ce537d6fbf8535dad08aa713
mkhodroo <info@behin.energy>
2024-06-23 18:32:26 +0330

    behin init v1.1

--- packages/behin-init/src/BehinInitProvider.php
+++ packages/behin-init/src/BehinInitProvider.php
@@ -26,7 +26,7 @@ class BehinInitProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/resources/layouts' => resource_path('views/behin-layouts'),
             __DIR__.'/views' => resource_path('views'),
-            // __DIR__. '/public' => public_path('behin'),
+            __DIR__. '/public' => public_path('behin'),
             __DIR__. '/routes' => 'routes',
             __DIR__. '/config' => config_path(),
         ]);
