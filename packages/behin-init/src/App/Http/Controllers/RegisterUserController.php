d81838e82853cb7bbb3044fd6ce42a05f6e90fc9
mkhodroo <info@behin.energy>
2024-07-06 14:19:20 +0330

    behin init package: v1.2

--- packages/behin-init/src/App/Http/Controllers/RegisterUserController.php
+++ packages/behin-init/src/App/Http/Controllers/RegisterUserController.php
@@ -33,6 +33,7 @@ class RegisterUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'digits:11', 'lowercase', 'unique:'.User::class],
             'password' => ['required', Rules\Password::defaults()],
+            'role_id' => config('init.default_role_id')
         ]);
 
         $user = User::create([
