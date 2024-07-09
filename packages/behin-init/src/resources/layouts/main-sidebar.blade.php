e0350282628c081de1095dd989236237145288ba
mkhodroo <info@behin.energy>
2024-07-09 20:28:02 +0330

    Update main-sidebar.blade.php

--- packages/behin-init/src/resources/layouts/main-sidebar.blade.php
+++ packages/behin-init/src/resources/layouts/main-sidebar.blade.php
@@ -26,7 +26,7 @@
                         @if ( access('منو >>' .$menu['fa_name']) )
                           <li class="nav-item has-treeview">
                             <a href="#" class="nav-link active">
-                                <i class="nav-icon fa fa-dashboard"></i>
+                                <i class="nav-icon @isset($menu['icon']) {{$menu['icon']}} @endisset"></i>
                                 <p>
                                     {{ $menu['fa_name'] }}
                                     <i class="right fa fa-angle-left"></i>
