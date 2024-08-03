a7110eb70c4de86889bdbc1d9534768bf1b4b32f
mkhodroo <info@behin.energy>
2024-08-03 18:04:07 +0330

    Update AllCasesController.php

--- packages/behin-process-maker-admin/src/Controllers/AllCasesController.php
+++ packages/behin-process-maker-admin/src/Controllers/AllCasesController.php
@@ -3,6 +3,7 @@
 namespace BehinProcessMakerAdmin\Controllers;
 
 use App\Http\Controllers\Controller;
+use BehinProcessMaker\Controllers\PMUserController;
 use BehinProcessMaker\Models\PmVars;
 
 class AllCasesController extends Controller
@@ -17,16 +18,17 @@ class AllCasesController extends Controller
         foreach($cases as $case){
             $search = PmVars::where('case_id', $case->case_id);
 
-            $customer_name = $search->where('key', 'customer_fullname')->first()?->value;
-            $receive_date = $search->where('key', 'receive_date')->first()?->value;
-            $repairman = $search->where('key', 'repairman')->first()?->value;
-            $status = $search->where('key', 'status')->first()?->value;
+            $customer_name = PmVars::where('case_id', $case->case_id)->where('key', 'customer_fullname')->first()?->value;
+            $receive_date = PmVars::where('case_id', $case->case_id)->where('key', 'receive_date')->first()?->value;
+            $repairman_id = PmVars::where('case_id', $case->case_id)->where('key', 'repairman')->first()?->value;
+            $repairman = PMUserController::getUserByPmUserId($repairman_id);
+            $status = PmVars::where('case_id', $case->case_id)->where('key', 'status')->first()?->value;
             $data[]= [
                 'process_id' => $case->process_id,
                 'case_id' => $case->case_id,
                 'customer_fullname' => $customer_name,
                 'receive_date' => $receive_date,
-                'repairman' => $repairman,
+                'repairman' => $repairman?->name,
                 'status' => $status,
             ];
         }
