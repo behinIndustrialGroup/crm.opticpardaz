0d04fcf15da7e9b48f48ebc60f2270fe3ee83544
mkhodroo <info@behin.energy>
2024-08-03 18:04:11 +0330

    Update PMUserController.php

--- packages/behin-process-maker/src/Controllers/PMUserController.php
+++ packages/behin-process-maker/src/Controllers/PMUserController.php
@@ -3,6 +3,7 @@
 namespace BehinProcessMaker\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use BehinProcessMaker\Models\PMUsers;
 use BehinProcessMaker\Models\PMVacation;
@@ -11,5 +12,9 @@ class PMUserController extends Controller
 {
     public static function getByName($user_name) {
         return PMUsers::where('USR_USERNAME', $user_name)->first();
+    }
+
+    public static function getUserByPmUserId($pm_user_uid){
+        return User::where('pm_user_uid', $pm_user_uid)->first();
     }
 }
