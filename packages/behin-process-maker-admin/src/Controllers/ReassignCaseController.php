babca9cd525d1a24a32c8eba21a99323bfc4a05f
mkhodroo <info@behin.energy>
2024-08-15 00:36:53 +0330

    Update ReassignCaseController.php

--- packages/behin-process-maker-admin/src/Controllers/ReassignCaseController.php
+++ packages/behin-process-maker-admin/src/Controllers/ReassignCaseController.php
@@ -14,10 +14,25 @@ use BehinProcessMaker\Controllers\TaskController;
 use BehinProcessMaker\Models\PmVars;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use SoapClient;
 
 class ReassignCaseController extends Controller
 {
     public static function reassign(Request $r){
+        $sessionId = AuthController::wsdl_login()->message;
+        $client = new SoapClient(str_replace('https', 'http', env('PM_SERVER')) . '/sysworkflow/en/green/services/wsdl2');
+        $params = array(array(
+            'sessionId' => $sessionId, 
+            'caseId' => $r->caseId, 
+            'delIndex' => $r->delIndex,
+            'userIdSource' => '884962377668917c92d7603066296900',
+            'userIdTarget' => '00000000000000000000000000000001'
+        ));
+        $result = $client->__SoapCall('reassignCase', $params);
+        if ($result->status_code != 0)
+            return response($result->message, 400);
+
+        return response("ok", 200);
         return ControllersReassignCaseController::reassign($r->caseId, '884962377668917c92d7603066296900', '00000000000000000000000000000001');
     }
 
