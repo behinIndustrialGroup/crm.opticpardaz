959944243ef7efd730edf1b909bc97931757fde3
mkhodroo <info@behin.energy>
2024-08-15 21:57:30 +0330

    Update pm_config.php

--- config/pm_config.php
+++ config/pm_config.php
@@ -6,6 +6,6 @@ return [
     'debug' => env('PM_DEBUG', false),
     'access_token_exp_in_minute' => 10,
     'send_sms_to_next_user' => true,
-    'execute_after_dynaform_triggers_in_save_form' => true,
+    'execute_after_dynaform_triggers_in_save_form' => false,
     'send_sms_to_next_user_text' => "کار جدید در کارتابل دارید.",
 ];
