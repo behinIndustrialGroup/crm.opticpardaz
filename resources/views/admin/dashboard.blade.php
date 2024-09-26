b04a7aa71c6dcb4d6a043965d4a8abcc44e483be
behinIndustrialGroup <info@behin.energy>
2024-09-26 18:50:26 +0330

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,13 @@
 @extends('behin-layouts.app')
+
+
+@section('content')
+<div class="row col-sm-12">
+    <div class="alert alert-danger col-sm-2 mt-3">
+        <a href="{{ route('MkhodrooProcessMaker.forms.todo') }}">
+            {{ trans('کارتابل من') }}
+        </a>
+    </div>
+</div>
+
+@endsection
