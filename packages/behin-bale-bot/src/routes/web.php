2de5d771ef7446fb552d68d9c54ee0892f1e0310
behinIndustrialGroup <info@behin.energy>
2025-01-12 18:35:56 +0330

    Update web.php

--- packages/behin-bale-bot/src/routes/web.php
+++ packages/behin-bale-bot/src/routes/web.php
@@ -6,4 +6,5 @@ use BaleBot\Controllers\BotController;
 
 Route::name('baleBot.')->prefix('bale-bot')->group(function(){
     Route::post('/chat', [BotController::class, 'chat'])->name('chat');
+    Route::get('/send-message', [BotController::class, 'sendMessage'])->name('send.message');
 });
