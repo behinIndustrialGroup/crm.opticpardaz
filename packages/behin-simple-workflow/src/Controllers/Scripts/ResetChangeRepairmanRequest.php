80a1ab3a5bdd1dd062252180fd95d728c529541c
behinIndustrialGroup <info@behin.energy>
2025-01-19 06:02:41 +0330

    Create ResetChangeRepairmanRequest.php

---
+++ packages/behin-simple-workflow/src/Controllers/Scripts/ResetChangeRepairmanRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Behin\SimpleWorkflow\Controllers\Scripts;
+
+use App\Http\Controllers\Controller;
+use Behin\SimpleWorkflow\Controllers\Core\CaseController;
+use Behin\SimpleWorkflow\Controllers\Core\InboxController;
+use Behin\SimpleWorkflow\Controllers\Core\ProcessController;
+use Behin\SimpleWorkflow\Controllers\Core\TaskController;
+use Behin\SimpleWorkflow\Controllers\Core\VariableController;
+use Behin\SimpleWorkflow\Models\Core\Process;
+use Behin\SimpleWorkflow\Models\Core\Task;
+use Behin\SimpleWorkflow\Models\Core\Variable;
+use Behin\SimpleWorkflow\Models\Entities\Repair_reports;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ResetChangeRepairmanRequest extends Controller
+{
+    private $case;
+    public function __construct($case)
+    {
+        $this->case = CaseController::getById($case->id);
+    }
+
+    public function execute()
+    {
+        return VariableController::save(
+            $this->case->process_id,
+            $this->case->id,
+            'change_repairman_request',
+            ''
+        );
+    }
+}
