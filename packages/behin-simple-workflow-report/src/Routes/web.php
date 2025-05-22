28dafef1327e23cc439cc4796f69a54076786f03
behinIndustrialGroup <info@behin.energy>
2025-05-22 11:11:33 +0330

    Update web.php

--- packages/behin-simple-workflow-report/src/Routes/web.php
+++ packages/behin-simple-workflow-report/src/Routes/web.php
@@ -15,6 +15,7 @@ use Behin\SimpleWorkflowReport\Controllers\Core\FinReportController;
 use Behin\SimpleWorkflowReport\Controllers\Core\ReportController;
 use Behin\SimpleWorkflowReport\Controllers\Core\RoleReportFormController;
 use Behin\SimpleWorkflowReport\Controllers\Core\SummaryReportController;
+use BehinProcessMaker\Controllers\VariableController;
 use BehinProcessMaker\Models\PMVariable;
 use BehinProcessMaker\Models\PmVars;
 use Illuminate\Support\Facades\Route;
@@ -26,38 +27,10 @@ Route::name('simpleWorkflowReport.')->prefix('workflow-report')->middleware(['we
     Route::resource('role', RoleReportFormController::class);
     Route::resource('fin-report', FinReportController::class);
     Route::get('total-payment', [FinReportController::class, 'totalPayment'])->name('totalPayment');
-    Route::get('import', function () {
-        $cases = PmVars::groupBy('case_id')->get();
-        foreach ($cases as $case) {
-            $number = PmVars::where('case_id', $case->case_id)->where('key', 'case_number')->first()?->value;
-            $number = $number ? $number : 1;
-            $creator = PmVars::where('case_id', $case->case_id)->where('key', 'crm_user_creator')->first()?->value;
-            $creator = $creator ? $creator : 1;
-            $name = PmVars::where('case_id', $case->case_id)->where('key', 'device_serial_no')->first()?->value;
-            $name = 'سریال نامبر: ' . $name;
-            $newCase = Cases::create([
-                'process_id' => '879e001c-59d5-4afb-958c-15ec7ff269d1',
-                'number' => $number,
-                'name' => $name,
-                'creator' => $creator
-            ]);
-            $vars = PmVars::where('case_id', $case->case_id)->get()->each(function ($row) use ($newCase) {
-                $row->process_id = '879e001c-59d5-4afb-958c-15ec7ff269d1';
-                $row->case_id = $newCase->id;
-            })->toArray();
-            foreach ($vars as $var) {
-                Variable::create($var);
-            }
+    Route::get('test', function () {
+        $images = Variable::where('key', 'device_plaque_image')->whereNotNull('value')->get();
+        foreach($images as $image){
+            echo "<a href='". url("public/$image->value")."' download>Download</a><br>";
         }
-        // $cases = Variable::where('key', 'case_number')->get();
-        // foreach($cases as $case){
-        //     $newCase = Cases::create([
-        //         'process_id' => $case->process_id,
-        //         'number' => $case->value ? $case->value : 1,
-        //         'name' => '',
-        //         'creator' => 1
-        //     ]);
-        //     Variable::where('case_id', $case->case_id)->update(['case_id' => $newCase->id]);
-        // }
     });
 });
