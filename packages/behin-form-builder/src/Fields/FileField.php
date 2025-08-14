936e694d411f0006b7e82e593b07e1a2766704d1
behinIndustrialGroup <info@behin.energy>
2025-08-14 16:57:25 +0330

    update

--- packages/behin-form-builder/src/Fields/FileField.php
+++ packages/behin-form-builder/src/Fields/FileField.php
@@ -4,13 +4,6 @@ namespace MyFormBuilder\Fields;
 
 class FileField extends AbstractField
 {
-    /**
-     * Renders the HTML for a file upload field, including a label, any existing file
-     * download links, and the file input element. Considers attributes like 
-     * 'required', 'readonly', and 'value' to customize the rendering.
-     * 
-     * @return string The generated HTML for the file upload field.
-     */
     public function render(): string
     {
         $s = '<div class="form-group">';
@@ -20,19 +13,13 @@ class FileField extends AbstractField
             $s .= ' <span class="text-danger">*</span>';
         }
         $s .= '</label><br>';
-        if($this->attributes['value'] && is_string($this->attributes['value'])){
-            $s .= '<a href="' . url('public/' . $this->attributes['value']) . '" target="_blank" download>' . trans('fields.Download') . '</a><br>';
+        foreach($this->attributes['value'] as $value){
+            $s .= '<a href="' . url('public/' . $value) . '" target="_blank" download>' . trans('fields.Download') . '</a><br>';
         }
-        if($this->attributes['value'] && is_array($this->attributes['value'])){
-            foreach($this->attributes['value'] as $value){
-                $s .= '<a href="' . url('public/' . $value) . '" target="_blank" download>' . trans('fields.Download') . '</a><br>';
-            }
-        }
-
         if($this->attributes['readonly'] == 'on'){
-            $s .= '<input type="file"  name="' . $this->name . '" disabled>';
+            $s .= '<input type="file" multiple name="' . $this->name . '" disabled>';
         }else{
-            $s .= '<input type="file" name="' . $this->name . '" ';
+            $s .= '<input type="file" multiple name="' . $this->name . '" ';
             foreach($this->attributes as $key => $value){
                 if($key != 'value'){
                     if($key == 'required'){
