94422fa5991d29a4bc32130d802f785ece7c98aa
behinIndustrialGroup <info@behin.energy>
2025-10-12 17:33:55 +0330

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ce20dbcb6c1f22ed34c92315a4f3beb31cbd1838',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -452,9 +452,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce20dbcb6c1f22ed34c92315a4f3beb31cbd1838',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
