925be035b164fe91e582753acc6548eeef847e0f
behinIndustrialGroup <info@behin.energy>
2025-12-11 16:07:26 +0330

    Update 8cc0f79c-11ac-4b60-bbe7-2444caff3776.blade.php

--- packages/behin-simple-workflow/src/Views/Custom/Form/8cc0f79c-11ac-4b60-bbe7-2444caff3776.blade.php
+++ packages/behin-simple-workflow/src/Views/Custom/Form/8cc0f79c-11ac-4b60-bbe7-2444caff3776.blade.php
@@ -1,4 +1,10 @@
 @php
-    
+    use Behin\SimpleWorkflow\Models\Entities\Case_customer;
+    use Behin\SimpleWorkflow\Models\Entities\Devices;
+    $customer = Case_customer::where('case_number', $case->number)->first();
+    $device = Devices::where('case_number', $case->number)->first();
+    return $customer;
 @endphp
+
+
 
