17ba043ca984b568fe31d9521ab274717d62f26d
behinIndustrialGroup <info@behin.energy>
2026-01-04 16:50:49 +0330

    Update show.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/AllRequests/show.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/AllRequests/show.blade.php
@@ -8,15 +8,15 @@
             اطلاعات مشتری
         </div>
         <div class="card-body">
-            <div>
+            <div class="col-sm-3">
                 <label for="">نام مشتری</label>
                 <p>{{ $case->customer->name }}</p>
             </div>
-            <div>
+            <div class="col-sm-3">
                 <label for="">موبایل مشتری</label>
                 <p>{{ $case->customer->mobile }}</p>
             </div>
-            <div>
+            <div class="col-sm-12">
                 <label for="">آدرس مشتری</label>
                 <p>{{ $case->customer->address }}</p>
             </div>
@@ -27,31 +27,31 @@
             اطلاعات دستگاه
         </div>
         <div class="card-body">
-            <div>
+            <div class="col-sm-3">
                 <label for="">نام دستگاه</label>
                 <p>{{ $case->device->name }}</p>
             </div>
-            <div>
+            <div class="col-sm-3">
                 <label for="">سری دستگاه</label>
                 <p>{{ $case->device->brand }}</p>
             </div>
-            <div>
+            <div class="col-sm-3">
                 <label for="">توان دستگاه</label>
                 <p>{{ $case->device->power }}</p>
             </div>
-            <div>
+            <div class="col-sm-3">
                 <label for="">سریال دستگاه</label>
                 <p>{{ $case->device->serial }}</p>
             </div>
-            <div>
+            <div class="col-sm-3">
                 <label for="">تصویر اولیه دستگاه</label>
                 <p>{{ $case->device->initial_pic }}</p>
             </div>
-            <div>
+            <div class="col-sm-3">
                 <label for="">تصویر پلاک دستگاه</label>
                 <p>{{ $case->device->plaque_pic }}</p>
             </div>
-            <div>
+            <div class="col-sm-3">
                 <label for="">مشخصات دستگاه</label>
                 <p>{{ $case->device->specifications }}</p>
             </div>
