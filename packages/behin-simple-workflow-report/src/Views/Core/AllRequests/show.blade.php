b645d866889a220b02dca90030133b7a1bec7cea
behinIndustrialGroup <info@behin.energy>
2026-01-04 16:49:52 +0330

    Update show.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/AllRequests/show.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/AllRequests/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'جزئیات درخواست')
 
 @section('content')
-    <div class="card">
+    <div class="card row">
         <div class="card-header">
             اطلاعات مشتری
         </div>
@@ -19,6 +19,41 @@
             <div>
                 <label for="">آدرس مشتری</label>
                 <p>{{ $case->customer->address }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="card row">
+        <div class="card-header">
+            اطلاعات دستگاه
+        </div>
+        <div class="card-body">
+            <div>
+                <label for="">نام دستگاه</label>
+                <p>{{ $case->device->name }}</p>
+            </div>
+            <div>
+                <label for="">سری دستگاه</label>
+                <p>{{ $case->device->brand }}</p>
+            </div>
+            <div>
+                <label for="">توان دستگاه</label>
+                <p>{{ $case->device->power }}</p>
+            </div>
+            <div>
+                <label for="">سریال دستگاه</label>
+                <p>{{ $case->device->serial }}</p>
+            </div>
+            <div>
+                <label for="">تصویر اولیه دستگاه</label>
+                <p>{{ $case->device->initial_pic }}</p>
+            </div>
+            <div>
+                <label for="">تصویر پلاک دستگاه</label>
+                <p>{{ $case->device->plaque_pic }}</p>
+            </div>
+            <div>
+                <label for="">مشخصات دستگاه</label>
+                <p>{{ $case->device->specifications }}</p>
             </div>
         </div>
     </div>
